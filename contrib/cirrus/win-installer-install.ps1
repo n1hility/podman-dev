6a7675d0c3ed40984212300eb889f01eef0f8213
Jason T. Greene <jason.greene@redhat.com>
2023-01-10 12:51:30 -0600

    test

--- contrib/cirrus/win-installer-install.ps1
+++ contrib/cirrus/win-installer-install.ps1
@@ -1,7 +1,12 @@
 # Update service is required for dotnet 3.5 (dep of wix)
 Set-Service -Name wuauserv -StartupType "Manual"
 #choco install -y wixtoolset mingw golang archiver
-& choco install -y blah
+
+$retries=0
+while (($LASTEXITCODE -ne 0) -and  ($retries -lt 5)) {
+  choco install -y blah
+  $retries++
+}
 if ($LASTEXITCODE -ne 0) {
    throw "Exit code failure = $LASTEXITCODE"
 }
