a45d85c340731d277ee24ad70df37871afcafb89
Jason T. Greene <jason.greene@redhat.com>
2023-01-10 13:23:07 -0600

    test

--- contrib/cirrus/win-installer-install.ps1
+++ contrib/cirrus/win-installer-install.ps1
@@ -3,19 +3,20 @@ Set-Service -Name wuauserv -StartupType "Manual"
 #choco install -y wixtoolset mingw golang archiver
 
 function retryInstall {
-   param($pkg)
+   param([Parameter(ValueFromRemainingArguments)] [string[]] $pkgs)
 
-   for ($retries=0;; $retries++) {
-     if ($retries -gt 5) {
-        throw "Could not install package $pkg"
+   foreach ($pkg in $pkgs) {
+     for ($retries=0;; $retries++) {
+        if ($retries -gt 5) {
+            throw "Could not install package $pkg"
+         }
+         choco install -y $pkg
+         if ($LASTEXITCODE -eq 0) {
+           break
+         }
+         Write-Output "Error installing, waiting before retry"
+         Start-Sleep -Seconds 6
      }
-     choco install -y $pkg
-     if (($LASTEXITCODE -eq 0) {
-        return
-     }
-     Write-Output "Error installing, waiting before retry"
-     Start-Sleep -Seconds 6
    }
 }
-retryInstall blah
-retryInstall mingw
+retryInstall blah mingw
